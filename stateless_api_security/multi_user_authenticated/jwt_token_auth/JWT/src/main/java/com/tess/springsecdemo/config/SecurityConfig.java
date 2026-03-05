908c89a0f2db34416e5e24aef91124476af2025e
tess <tesfa.py@gmail.com>
2026-03-04 21:28:51 -0500

    db bcrypt and jwt auth for stateless api

--- stateless_api_security/multi_user_authenticated/jwt_token_auth/JWT/src/main/java/com/tess/springsecdemo/config/SecurityConfig.java
+++ stateless_api_security/multi_user_authenticated/jwt_token_auth/JWT/src/main/java/com/tess/springsecdemo/config/SecurityConfig.java
@@ -4,15 +4,17 @@ import org.springframework.beans.factory.annotation.Autowired;
 
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.security.authentication.AuthenticationManager;
 import org.springframework.security.authentication.AuthenticationProvider;
 import org.springframework.security.authentication.dao.DaoAuthenticationProvider;
-import org.springframework.security.config.Customizer;
+import org.springframework.security.config.annotation.authentication.configuration.AuthenticationConfiguration;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.http.SessionCreationPolicy;
 import org.springframework.security.core.userdetails.UserDetailsService;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.security.web.SecurityFilterChain;
+import org.springframework.security.web.authentication.UsernamePasswordAuthenticationFilter;
 
 @Configuration
 @EnableWebSecurity
@@ -20,55 +22,50 @@ public class SecurityConfig {
 
 	@Autowired
 	private UserDetailsService userDetailsService;
-	
+
+	@Autowired
+	private JwtFilter jwtFilter;
 	
 	@Bean
 	public AuthenticationProvider authProvider() {
-		DaoAuthenticationProvider provider=new DaoAuthenticationProvider();
-		provider.setUserDetailsService(userDetailsService);
+        // Setter method of DaoAuthenticationProvider is Deprecated.
+        // Used Constructor.
+		DaoAuthenticationProvider provider=new DaoAuthenticationProvider(userDetailsService);
 		provider.setPasswordEncoder(new BCryptPasswordEncoder(12));
 		return provider;
 	}
-
+	
 
 	@Bean
 	public SecurityFilterChain securityFilterChain(HttpSecurity http) throws Exception {
+
 		http.csrf(customizer -> customizer.disable())
 				.authorizeHttpRequests(request -> request
-						.requestMatchers("register")
+						.requestMatchers("register", "login")
 						.permitAll()
 						.anyRequest().authenticated())
-				.sessionManagement(session ->
-						session.sessionCreationPolicy(SessionCreationPolicy.STATELESS));
+				.sessionManagement(session -> session.sessionCreationPolicy(SessionCreationPolicy.STATELESS))
+				.addFilterBefore(jwtFilter, UsernamePasswordAuthenticationFilter.class);
+
 		return http.build();
 	}
+
+	@Bean
+	public AuthenticationManager authenticationManager(AuthenticationConfiguration config) throws Exception {
+		return config.getAuthenticationManager();
+	}
 	
-	
-//	@Bean
-//	public SecurityFilterChain securityFilterChain(HttpSecurity http) throws Exception {
-//
-//		http.csrf(customizer -> customizer.disable())
-//				.authorizeHttpRequests(request -> request
-//						.requestMatchers("register")
-//						.permitAll().anyRequest().authenticated())
-////				.httpBasic(Customizer.withDefaults())
-//				.sessionManagement(session -> session.sessionCreationPolicy(SessionCreationPolicy.STATELESS));
-//
-//		return http.build();
-//	}
-//
-//
 	
 	
 	
 	/*
 	 * @Bean public UserDetailsService userDetailsService() {
 	 * 
-	 * UserDetails user=User .withDefaultPasswordEncoder() .username("navin")
-	 * .password("n@123") .roles("USER") .build();
+	 * UserDetails user=User .withDefaultPasswordEncoder() .username("tess")
+	 * .password("tessp") .roles("USER") .build();
 	 * 
 	 * UserDetails admin=User .withDefaultPasswordEncoder() .username("admin")
-	 * .password("admin@789") .roles("ADMIN") .build();
+	 * .password("adminp") .roles("ADMIN") .build();
 	 * 
 	 * return new InMemoryUserDetailsManager(user,admin); }
 	 */
